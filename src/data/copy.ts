4c21fac73bcc023b3151de640d44b35f30e4a9f8
류다은 <ryudahlia@yonsei.ac.kr>
2026-05-14 19:39:37 +0900

    second commit

--- src/data/copy.ts
+++ src/data/copy.ts
@@ -5,7 +5,7 @@ export const HERO_TITLE = '프론트엔드처럼 멋쟁이사자';
 export const HERO_SUBTITLE = '함께 만들고, 같이 PR을 날리고, 충돌을 해결하면서 자라는 동아리.';
 export const HERO_CTA = '활동 보러가기';
 
-export const ABOUT_HEADLINE = '코드로 함께 자라는 사람들';
+export const ABOUT_HEADLINE = '코드로 사람들';
 export const ABOUT_BODY =
   '멋쟁이사자처럼은 누구나 시작할 수 있고, 누구와도 협업할 수 있는 프론트엔드 동아리입니다. 우리는 PR을 두려워하지 않습니다.';
 
